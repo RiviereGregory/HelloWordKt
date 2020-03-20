b58bc1fedbdcd7355b698b3eb35422c530ac78ca
RIVIERE <gregory.riviere@laposte.net>
2020-03-20 10:32:46 +0100

    Ajout SwipeRefreshBar

--- app/src/main/java/training/android/UserDetailActivity.kt
+++ app/src/main/java/training/android/UserDetailActivity.kt
@@ -1,15 +1,29 @@
 package training.android
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 
 class UserDetailActivity : AppCompatActivity() {
+    private lateinit var swipeRefreshLayout: SwipeRefreshLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_detail)
+
+        swipeRefreshLayout = findViewById(R.id.swiperefresh)
+        swipeRefreshLayout.setOnRefreshListener {
+            schedulRefreshStop()
+        }
+
+        findViewById<Button>(R.id.refresh).setOnClickListener {
+            swipeRefreshLayout.isRefreshing = true
+            schedulRefreshStop()
+        }
 
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
@@ -24,5 +38,10 @@ class UserDetailActivity : AppCompatActivity() {
         ageTextView.setText("Age: ${user.age}")
 
 
+    }
+
+    private fun schedulRefreshStop() {
+        var handler = Handler()
+        handler.postDelayed({ swipeRefreshLayout.isRefreshing = false }, 2000)
     }
 }
